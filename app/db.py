dbcdc7dd74d7295d3a2eeef1983f96e826a10284
Jisha Obukwelu <jisha@obukwelu.com>
2021-03-06 06:06:10 -0500

    Update Database url

--- app/db.py
+++ app/db.py
@@ -14,7 +14,7 @@ from pypika.terms import Field
 Field_ = Union[Field, str]
 
 load_dotenv()
-database_url = os.getenv("HEROKU_POSTGRESQL_SILVER_URL", default = "DATABASE_URL")
+database_url = os.getenv("DATABASE_URL")
 database = databases.Database(database_url)
 
 router = APIRouter()
