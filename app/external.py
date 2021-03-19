dcba43651dda1b8f084c0058079099cd6c9ee811
Jisha Obukwelu <jisha@obukwelu.com>
2021-03-18 21:44:40 -0400

    update weather api

--- app/external.py
+++ app/external.py
@@ -12,7 +12,7 @@ weather_api = os.getenv("WEATHER_API_KEY")
 
 @router.post('/api/temperature')
 
-# 1
+# 1 https://www.w3resource.com/python-exercises/web-scraping/web-scraping-exercise-21.php
 def temperature():
     query = request.form['q']
     r = requests.get('http://api.openweathermap.org/data/2.5/weather?q='+query+',us&appid='+weather_api)
@@ -21,7 +21,7 @@ def temperature():
     temp_f = (temp_k - 273.15) * 1.8 + 32
     return {"current temperature": temp_f}
 
-# 2
+# 2  https://github.com/PrettyPrinted/python-weather-api/blob/master/test.py
 def weather_data(query):
 	res=requests.get('http://api.openweathermap.org/data/2.5/weather?'+query+',us&appid='+weather_api);
 	return res.json()
@@ -45,6 +45,80 @@ def main():
 
 if __name__=='__main__':
 	main()
+
+import requests
+import datetime
+import time
+
+cronjob = True
+
+# 3 https://github.com/juhilsomaiya/API-Integrations-Python/blob/master/Weather_forecast/main.py
+def get_details():
+    city_name = input("Enter city name")
+    country_code = input("Enter country code")
+    return {'city': city_name, 'country_code': country_code}
+
+
+def get_data():
+    app_id = 'Replace with yours'
+    details = get_details()
+    city_name = details.get('city')
+    country_code = details.get('country_code')
+    api_call = 'http://api.openweathermap.org/data/2.5/forecast?q=' + city_name + ',' + country_code + '&appid=' + app_id + '&mode=json&units=metric'
+
+    data = requests.post(api_call)
+    humidity = []
+    date = []
+    temp = []
+    desc = []
+    pressure = []
+    sea_level = []
+
+    data = data.json()
+    print("\t" + "Details about the forcast of the city " + city_name + " of next 5 days " + "\n\n")
+    for lists in data['list']:
+        date.append(lists['dt_txt'])
+        humidity.append(lists['main']['humidity'])
+        temp.append(lists['main']['temp'])
+        pressure.append(lists['main']['pressure'])
+        sea_level.append(lists['main']['sea_level'])
+        desc.append(lists['weather'][0]['description'])
+
+    print('{:^25}{:^20}{:^20}{:^20}{:^20}{:^20}'.format("Date", "Description", "Temprature", "Humidity", "Pressure", "Sea_level\n"))
+    for i in range(len(humidity)):
+        print('{:^25}{:^20}{:^20}{:^20}{:^20}{:^20}'.format(str(date[i]), desc[i], str(temp[i])+" C\N{DEGREE SIGN}", str(humidity[i])+" %",
+            str(pressure[i])+" hPa", str(sea_level[i])+" hPa"))
+
+    with open("test.txt", "a+") as file:
+        date = datetime.datetime.now().strftime("%H:%M:%S")
+        file.write('Logged data at: ' + str(date) + '\n')
+
+    if cronjob:
+        time.sleep(5)
+        get_data()
+
+
+get_data()
+
+# 4 afternerd
+class WeatherParams:
+    def __init__(self, city, units="imperial", lang="en"):
+        self.city = city
+        self.units = units
+        self.lang = lang
+        self.mode = "json"
+
+def get_weather(appid, weather_params):
+    endpoint = "http://api.openweathermap.org/data/2.5/weather"
+    query_params = {
+        'q': weather_params.city,
+        'units': weather_params.units,
+        'lang': weather_params.lang,
+        'mode': weather_params.mode,
+        'appid': appid,
+    }
+    resp = requests.get(endpoint, params=query_params)
+    return resp.json
 
 @router.post("/api/walkability")
 async def get_walkability(city: City):
