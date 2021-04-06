04b8736df74ed87bd36f85e31f86195f58dfb145
Jisha Obukwelu <jisha@obukwelu.com>
2021-04-06 11:44:20 -0400

    Update rental api

--- app/external.py
+++ app/external.py
@@ -206,29 +206,47 @@ async def rental_listing(
         line = response[i]['location']['address']['line']
         city = response[i]['location']['address']['city']
         state = response[i]['location']['address']['state']
-        baths = response[i]['description']['baths_max']
-        bedrooms = response[i]['description']['beds_max']
-        cats_allowed = response[i]['pet_policy']['cats']
-        dogs_allowed = response[i]['pet_policy']['dogs']
+        try:
+            baths = response[i]['description']['baths_max']
+        except AttributeError:
+            baths = 0
+        try:
+            bedrooms = response[i]['description']['beds_max']
+        except AttributeError:
+            bedrooms = 0
+        try:
+            cats_allowed = response[i]['pet_policy']['cats']
+        except AttributeError:
+            cats_allowed = False
+        try:
+            dogs_allowed = response[i]['pet_policy']['dogs']
+        except AttributeError:
+            dogs_allowed = False
         list_price = response[i]['list_price_max']
-        ammenities = response[i]['tags']
-        photos = response[i]['photos']
+        try:
+            ammenities = response[i]['tags']
+        except AttributeError:
+            ammenities = []
+        try:
+            photos = response[i]['photos']
+        except AttributeError:
+            photos = None
 
-    element={
-        'Latitude': lat,
-        'Longitude': lon,
-        'Street Address': line,
-        'City': city,
-        'State': state,
-        'Bedrooms': bedrooms,
-        'Bathrooms': baths,
-        'Cats Allowed': cats_allowed,
-        'Dogs Allowed': dogs_allowed,
-        'List Price': list_price,
-        'Ammenities': ammenities,
-        'Photos': photos,
+        elements={
+            'Latitude': lat,
+            'Longitude': lon,
+            'Street Address': line,
+            'City': city,
+            'State': state,
+            'Bedrooms': bedrooms,
+            'Bathrooms': baths,
+            'Cats Allowed': cats_allowed,
+            'Dogs Allowed': dogs_allowed,
+            'List Price': list_price,
+            'Ammenities': ammenities,
+            'Photos': photos,
       }
-
-    rental_list.append(element)
+        rental_list.append(elements)
 
     return rental_list
+
